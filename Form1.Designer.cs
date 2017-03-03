63726199adf40cd321dbc2cd0360bad7838857ca
alex2345 <atest2345@gmail.com>
2017-03-03 16:16:02 +0800

    version 1 (win.access) ...

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace win_access
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.資料庫1DataSet = new win_access.資料庫1DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.snoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.snameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentTableAdapter = new win_access.資料庫1DataSetTableAdapters.studentTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.gidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.snoDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scoreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentgradeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gradeTableAdapter = new win_access.資料庫1DataSetTableAdapters.gradeTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.資料庫1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentgradeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this.資料庫1DataSet;
+            this.bindingSource1.Position = 0;
+            // 
+            // 資料庫1DataSet
+            // 
+            this.資料庫1DataSet.DataSetName = "資料庫1DataSet";
+            this.資料庫1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.snoDataGridViewTextBoxColumn,
+            this.snameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.studentBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(253, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // snoDataGridViewTextBoxColumn
+            // 
+            this.snoDataGridViewTextBoxColumn.DataPropertyName = "sno";
+            this.snoDataGridViewTextBoxColumn.HeaderText = "sno";
+            this.snoDataGridViewTextBoxColumn.Name = "snoDataGridViewTextBoxColumn";
+            // 
+            // snameDataGridViewTextBoxColumn
+            // 
+            this.snameDataGridViewTextBoxColumn.DataPropertyName = "sname";
+            this.snameDataGridViewTextBoxColumn.HeaderText = "sname";
+            this.snameDataGridViewTextBoxColumn.Name = "snameDataGridViewTextBoxColumn";
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataMember = "student";
+            this.studentBindingSource.DataSource = this.bindingSource1;
+            // 
+            // studentTableAdapter
+            // 
+            this.studentTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gidDataGridViewTextBoxColumn,
+            this.snoDataGridViewTextBoxColumn1,
+            this.courseDataGridViewTextBoxColumn,
+            this.scoreDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.studentgradeBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 156);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(464, 188);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // gidDataGridViewTextBoxColumn
+            // 
+            this.gidDataGridViewTextBoxColumn.DataPropertyName = "gid";
+            this.gidDataGridViewTextBoxColumn.HeaderText = "gid";
+            this.gidDataGridViewTextBoxColumn.Name = "gidDataGridViewTextBoxColumn";
+            // 
+            // snoDataGridViewTextBoxColumn1
+            // 
+            this.snoDataGridViewTextBoxColumn1.DataPropertyName = "sno";
+            this.snoDataGridViewTextBoxColumn1.HeaderText = "sno";
+            this.snoDataGridViewTextBoxColumn1.Name = "snoDataGridViewTextBoxColumn1";
+            // 
+            // courseDataGridViewTextBoxColumn
+            // 
+            this.courseDataGridViewTextBoxColumn.DataPropertyName = "course";
+            this.courseDataGridViewTextBoxColumn.HeaderText = "course";
+            this.courseDataGridViewTextBoxColumn.Name = "courseDataGridViewTextBoxColumn";
+            // 
+            // scoreDataGridViewTextBoxColumn
+            // 
+            this.scoreDataGridViewTextBoxColumn.DataPropertyName = "score";
+            this.scoreDataGridViewTextBoxColumn.HeaderText = "score";
+            this.scoreDataGridViewTextBoxColumn.Name = "scoreDataGridViewTextBoxColumn";
+            // 
+            // studentgradeBindingSource
+            // 
+            this.studentgradeBindingSource.DataMember = "studentgrade";
+            this.studentgradeBindingSource.DataSource = this.studentBindingSource;
+            // 
+            // gradeTableAdapter
+            // 
+            this.gradeTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Add CheckBox";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Remove CheckBox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 356);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "win_access";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.資料庫1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentgradeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private 資料庫1DataSet 資料庫1DataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private 資料庫1DataSetTableAdapters.studentTableAdapter studentTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.BindingSource studentgradeBindingSource;
+        private 資料庫1DataSetTableAdapters.gradeTableAdapter gradeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snoDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scoreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win_access
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO:  這行程式碼會將資料載入 '資料庫1DataSet.grade' 資料表。您可以視需要進行移動或移除。
+            this.gradeTableAdapter.Fill(this.資料庫1DataSet.grade);
+            // TODO:  這行程式碼會將資料載入 '資料庫1DataSet.student' 資料表。您可以視需要進行移動或移除。
+            this.studentTableAdapter.Fill(this.資料庫1DataSet.student);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // [C#][WinForm] GridView 中 CheckBox 全選
+            //https://dotblogs.com.tw/shunnien/2013/07/22/111941
+            //step1. 先建立 CheckBox 欄
+            DataGridViewCheckBoxColumn cbCol = new DataGridViewCheckBoxColumn();
+            cbCol.Name = "chkbox_0";     //給定名稱
+            cbCol.Width = 50;                      //設定寬度
+            cbCol.HeaderText = "　全選"; //標題文字
+            cbCol.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;   //置中
+            dataGridView2.Columns.Insert(0, cbCol);
+            //
+            //step2. 建立矩形，等下
+            Rectangle rect = dataGridView2.GetCellDisplayRectangle(0, -1, true);
+            rect.X = rect.Location.X + rect.Width / 4 - 9;     //CheckBox 嵌入 GridView 的位置 (X軸)
+            rect.Y = rect.Location.Y + (rect.Height / 2 - 14); //CheckBox 嵌入 GridView 的位置 (Y軸)
+            CheckBox cbHeader = new CheckBox();
+            cbHeader.Name = "checkboxHeader";
+            cbHeader.Size = new Size(18, 18);
+            cbHeader.Location = rect.Location;    
+            //委派 delegate 全選要設定的事件
+            cbHeader.CheckedChanged += new EventHandler(cbHeader_CheckedChanged);
+            //將 CheckBox 加入到 dataGridView
+            dataGridView2.Controls.Add(cbHeader);
+            
+        }
+        private void cbHeader_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow dr in dataGridView2.Rows)
+                dr.Cells[0].Value = ((CheckBox)dataGridView2.Controls.
+                    Find("checkboxHeader", true)[0]).Checked;
+            // dr.Cells[0] = 設定 DataGridView 第0欄的值
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Remove("chkbox_0");
+        }
+    }
+}
